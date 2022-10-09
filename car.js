c93e51dc1928dc1194ba544356eade4686758470
sbx_user1051 <sbx_user1051@169.254.39.225>
2022-10-09 19:15:42 +0000

    Initial commit

---
+++ car.js
@@ -0,0 +1,20 @@
+    /**
+     * This is the function where you need to write your code solution
+     * "roadDamages" is defined down below, where the function gets called
+     */
+    function moveCar(roadDamages) {
+       // TODO Implement the solution here
+    }
+
+    /**
+     * Function gets called
+     * You can call this function with different set of array values in order to test different scenarios
+     * Do not change the amount of values inside the array
+     */
+    moveCar([false, false, false, false, true, false, true, false, false, false]);
+
+    /**
+     * This code line is required in order to trigger automated tests
+     * Do not change this code line
+     */
+    export default moveCar;
