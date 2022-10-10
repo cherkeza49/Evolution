c2d37a1d2f62f36eb0ffe806f12d058e8bffcab2
cherkeza49 <115451590+cherkeza49@users.noreply.github.com>
2022-10-10 15:55:44 +0400

    Update car.js

--- car.js
+++ car.js
@@ -1,20 +1,36 @@
-    /**
-     * This is the function where you need to write your code solution
-     * "roadDamages" is defined down below, where the function gets called
-     */
-    function moveCar(roadDamages) {
-       // TODO Implement the solution here
+   //this is a road
+var road= [false, false, false, false, true, false, false, false, true, false];
+
+
+function start(){
+     console.log(moveCar(road));
+    
+}
+//determines if the car is broken or just damaged 
+
+function moveCar(roadWithDamages) {
+    var i = 0;
+    var length = (roadWithDamages && roadWithDamages.length) || 0;
+    var isCarDamaged = false;
+    var broke = "The car is broken";
+    while (i < length) {
+        if (roadWithDamages[i]) {
+            if (isCarDamaged) {
+                console.log("The car is broken");
+                return;
+            }
+
+            isCarDamaged = true;
+            console.log("The car is damaged");
+
+            i++;
+            continue;
+        }
+        
+        i++;
+        console.log("The car goes to step " + i);
     }
-
-    /**
-     * Function gets called
-     * You can call this function with different set of array values in order to test different scenarios
-     * Do not change the amount of values inside the array
-     */
-    moveCar([false, false, false, false, true, false, true, false, false, false]);
-
-    /**
-     * This code line is required in order to trigger automated tests
-     * Do not change this code line
-     */
-    export default moveCar;
+    if( isCarDamaged = true) {
+        return;
+    }
+}
